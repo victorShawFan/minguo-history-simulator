295cddf2d4d09d23f227c7796d488a527ead13d0
肖钒 <xiaofan.v@bytedance.com>
2026-03-04 14:48:58 +0800

    R24: 持续优化

---
+++ player.py
@@ -0,0 +1,17 @@
+# 玩家系统
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.decisions = []
+        self.achievements = []
+        
+    def make_decision(self, event, choice):
+        self.decisions.append({
+            'event': event,
+            'choice': choice
+        })
+        
+    def unlock_achievement(self, achievement):
+        self.achievements.append(achievement)
+# Round 24
